c6fe266aaa39d34450bd569af723ab2b0deed3d3
MaksimChesnov <machesnov@gmail.com>
2025-01-20 18:06:07 +0300

    Парсинг музыки.Уже работает. Надо причесать

---
+++ les_pars_music.py
@@ -0,0 +1,44 @@
+import pip._vendor.requests as req
+import os
+
+#Подключение к сайту с музыкой
+res = req.get('https://rus.hitmotop.com')
+
+if res.status_code == 200:
+    print('Подключение успешно, ', res.status_code)
+    site_txt = res.text
+    find_link = 'https://'
+    find_mus_file = '.mp3'
+
+    #Разбиваем полученный текст сайта на элементы списка
+    spl_site_txt = site_txt.split()
+
+    #Заполняем новый список элементами, которые содержат ссылку и музыкальный файл
+    links = []
+    for i in spl_site_txt:
+        if find_link in i:
+            if find_mus_file in i:
+                links.append(i)
+
+    print (links)
+
+    #У нас есть список ссылок. Идем по ним и выкачиваем файлы
+    ind = 0
+    for i in links[0:5]:
+        #Убираем href= из ссылки
+        link = links[ind].replace('href=', '')
+        #Убираем двойные кавычки
+        link = link.replace('"', '')
+        
+        ind = ind + 1
+        #Берем чистую ссылку
+        req_link = req.get(link)
+        dirname, filename = os.path.split(link)
+        out = open(filename, 'wb')
+        out.write(req_link.content)
+        out.close()
+        print (req_link.status_code, ',', link)
+                
+else:
+    print('Не получилось подключиться, код ошибки = ', res.status_code)
+     
